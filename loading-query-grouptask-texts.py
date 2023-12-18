30576c2c53a261cc264df03f249dfbf14f4c9c92
glauberss2007 <glauberss2007@gmail.com>
2023-12-18 18:08:02 -0300

    Renamed the file name

--- loading-query-grouptask-texts.py
+++ loading-query-grouptask-texts.py
@@ -51,8 +51,52 @@ print(response.objects[0].properties)
 
 #response = questions.aggregate.over_all()
 #print(response)
+#questions.data.delete_by_id()
 
-# Quering data
+# Vector search
+query_response = questions.query.near_text(
+    query="pigments",
+    limit = 5)
 
+for item in query_response:
+    print(item)
+    print(item.properties)
+
+# Searching with filters
+questions = client.collections.get("Questions")
+
+query_response_search_filtered =questions.query.near_text(
+    query="musical instruments",
+    limit = 5,
+    filters = weaviate.classes.Filter("value").greater_than(500)
+)
+
+for item in query_response_search_filtered:
+    print(item)
+    print(item.properties)
+
+# Hybrid search
+query_response_search_hibrid = questions.query.hybrid(
+    query = "musical instruments",
+    alpha = 0.7, # 70% vector search 30% keyword search
+    limit = 5)
+
+for item in query_response_search_hibrid:
+    print(item)
+    print(item.properties)
+
+# Group task that search an word using vector search and them create a simple tweet about it using chat GPT4
+group_task_generate = client.collections.get("Questions")
+
+response_group_task_generate = questions.generate.near_text(
+    query="musical instruments",
+    limit = 4,
+    single_prompt = f"Write a short tweet about: {questions}"
+)
+
+for item in response_group_task_generate:
+    print(item)
+    print(item.properties)
+    print(response_group_task_generate.generated)
 
 
